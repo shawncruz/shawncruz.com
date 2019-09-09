dea5612ad50d68405380be4667b1a08da882c9a7
shawncruz <shawncruz3991@gmail.com>
2019-09-08 23:48:37 -0400

    Remove boilerplate

--- src/components/ArticleMeta.js
+++ src/components/ArticleMeta.js
@@ -1,43 +1,44 @@
-import { join } from 'path'
-import React from 'react'
-import { Link } from 'react-navi'
-import { formatDate } from '../utils/formats'
-import styles from './ArticleMeta.module.css'
+import { join } from "path";
+import React from "react";
+import { Link } from "react-navi";
+import { formatDate } from "../utils/formats";
+import styles from "./ArticleMeta.module.css";
 
 function ArticleMeta({ blogRoot, meta, readingTime }) {
-  let readingTimeElement
-  if (readingTime) {
-    let minutes = Math.max(Math.round(readingTime.minutes), 1)
-    let cups = Math.round(minutes / 5);
-    readingTimeElement =
-      <React.Fragment>
-        {' '}&bull;{' '}
-        <span className={styles.readingTime}>
-          {new Array(cups || 1).fill('☕️').join('')} {minutes} min read
-        </span>
-      </React.Fragment>
-  }
-      
+  // let readingTimeElement;
+  // if (readingTime) {
+  //   let minutes = Math.max(Math.round(readingTime.minutes), 1);
+  //   let cups = Math.round(minutes / 5);
+  //   readingTimeElement = (
+  //     <React.Fragment>
+  //       {" "}
+  //       &bull;{" "}
+  //       <span className={styles.readingTime}>
+  //         {new Array(cups || 1).fill("☕️").join("")} {minutes} min read
+  //       </span>
+  //     </React.Fragment>
+  //   );
+  // }
+
   return (
     <small className={styles.ArticleMeta}>
       <time dateTime={meta.date.toUTCString()}>{formatDate(meta.date)}</time>
-      {
-        meta.tags &&
-        meta.tags.length &&
+      {meta.tags && meta.tags.length && (
         <>
-          {' '}&bull;{' '}
+          {" "}
+          &bull;{" "}
           <ul className={styles.tags}>
-            {meta.tags.map(tag =>
+            {meta.tags.map(tag => (
               <li key={tag}>
-                <Link href={join(blogRoot, 'tags', tag)}>{tag}</Link>
+                <Link href={join(blogRoot, "tags", tag)}>{tag}</Link>
               </li>
-            )}
+            ))}
           </ul>
         </>
-      }
-      {readingTimeElement || null}
+      )}
+      {/* {readingTimeElement || null} */}
     </small>
-  )
+  );
 }
 
-export default ArticleMeta
+export default ArticleMeta;
--- src/components/BlogIndexPage.js
+++ src/components/BlogIndexPage.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Link } from 'react-navi'
-import siteMetadata from '../siteMetadata'
-import ArticleSummary from './ArticleSummary'
-import Bio from './Bio'
-import Pagination from './Pagination'
-import styles from './BlogIndexPage.module.css'
+import React from "react";
+import { Link } from "react-navi";
+import siteMetadata from "../siteMetadata";
+import ArticleSummary from "./ArticleSummary";
+import Bio from "./Bio";
+import Pagination from "./Pagination";
+import styles from "./BlogIndexPage.module.css";
 
 function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
   return (
@@ -16,41 +16,31 @@ function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
         <Bio />
       </header>
       <ul className={styles.articlesList}>
-        {postRoutes.map(route =>
+        {postRoutes.map(route => (
           <li key={route.url.href}>
             <ArticleSummary blogRoot={blogRoot} route={route} />
           </li>
-        )}
+        ))}
       </ul>
-      {
-        pageCount > 1 &&
+      {pageCount > 1 && (
         <Pagination
           blogRoot={blogRoot}
           pageCount={pageCount}
           pageNumber={pageNumber}
         />
-      }
+      )}
       <footer className={styles.footer}>
         <div>
-          <a
-            href='./rss.xml'
-            target='_blank'
-            style={{ float: 'right' }}>
+          <a href="./rss.xml" target="_blank" style={{ float: "right" }}>
             RSS
           </a>
-          <Link href='./about'>
-            About
-          </Link> &bull;{' '}
-          <Link href='./tags'>
-            Tags
-          </Link> &bull;{' '}
-          <a href='https://github.com/frontarm/create-react-blog'>
-            Source
-          </a>
+          <Link href="./about">About</Link> &bull;{" "}
+          <Link href="./tags">Tags</Link> &bull;{" "}
+          <a href="https://github.com/shawncruz/personal-blog">Source</a>
         </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default BlogIndexPage
+export default BlogIndexPage;
