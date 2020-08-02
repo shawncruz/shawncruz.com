784b7c4935bc53a370e72c41e0a880d2a310ae9d
shawncruz <shawncruz3991@gmail.com>
2020-08-01 23:28:31 -0400

    Small wording updates; rename post name

---
+++ src/routes/posts/2019-11-29-continuous-website-deployment/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Continuous Website Deployment`,
+  spoiler:
+    "How I continuously deploy changes to my personal website.",
+  getContent: () => import("./document.mdx")
+};
--- src/routes/posts/2019-11-29-deploying-my-personal-website/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Website Deployment and Beyond`,
-  spoiler:
-    "How I easily deploy changes to my website.",
-  getContent: () => import("./document.mdx")
-};
