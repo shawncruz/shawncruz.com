0aaefb6175d72d56fd57302b2014f79827eaaf21
shawncruz <shawncruz3991@gmail.com>
2023-11-26 16:14:30 -0500

    2023-11-25-postgresql-update-gotcha

---
+++ src/routes/posts/2023-11-25-postgresql-update-gotcha/post.js
@@ -0,0 +1,5 @@
+export default {
+  title: `PostgreSQL UPDATE Gotcha`,
+  spoiler: "A first hand experience with pitfalls in a declarative language",
+  getContent: () => import("./document.mdx")
+};
